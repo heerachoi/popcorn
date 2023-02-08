8822e471e6f85bc4c4169fb7482ef4fcf272afe5
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-02-08 16:48:33 +0900

    feat: 로그인/회원가입/로그아웃/회원탈퇴 기능 완성2

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUpPage from '../pages/SignUpPage/SignUpPage';
+import SignupPage from '../pages/SignUpPage/SignUpPage';
 import MyPage from '../pages/MyPage/MyPage';
 import DetailPage from '../pages/DetailPage/DetailPage';
 import HomePage from '../pages/HomePage/HomePage';
+import SearchPage from '../pages/SearchPage/SearchPage';
 
 const Router = () => {
   return (
@@ -10,8 +11,9 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/detail" element={<DetailPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/my" element={<MyPage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
   );
