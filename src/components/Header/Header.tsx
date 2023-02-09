e6547779a89623839a3deb5fe930ed5837b19edc
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-02-09 19:38:18 +0900

    fix: 헤더 navigate 함수

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,32 @@
-import { Outlet, useNavigate } from 'react-router-dom';
 import * as S from './style';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase';
 import SignOut from '../Signup/SignOut';
 
 const Header = () => {
   const navigate = useNavigate();
+  const changePageHandler = (page: string) => navigate(page);
 
   return (
     <>
       <S.Wrap>
-        <S.Title onClick={() => navigate('/')}>POPCORN</S.Title>
+        <S.Title onClick={() => changePageHandler('/')}>POPCORN</S.Title>
         <S.DummyBox></S.DummyBox>
         <S.BtnWrap>
-          <S.CustomerCenterBtn onClick={() => navigate('/customer')}>
+          <S.CustomerCenterBtn onClick={() => changePageHandler('/customer')}>
             고객센터
           </S.CustomerCenterBtn>
-          <S.SignUpBtn onClick={() => navigate('/signup')}>
+          <S.SignUpBtn onClick={() => changePageHandler('/signup')}>
             회원가입
           </S.SignUpBtn>
           {!auth?.currentUser?.uid ? (
-            <S.LoginBtn onClick={() => navigate('/login')}>로그인</S.LoginBtn>
+            <S.LoginBtn onClick={() => changePageHandler('/login')}>
+              로그인
+            </S.LoginBtn>
           ) : (
             <SignOut />
           )}
-          <S.MapBtn onClick={() => navigate('/map')}>지도</S.MapBtn>
+          <S.MapBtn onClick={() => changePageHandler('/map')}>지도</S.MapBtn>
         </S.BtnWrap>
       </S.Wrap>
       <Outlet />
