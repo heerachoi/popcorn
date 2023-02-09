41ae917c7ee451dbc351fb5ce365150aaf24f022
Jieun Han <yjyyls66@naver.com>
2023-02-09 15:53:32 +0900

    Feat: 자주 묻는 질문 db 생성

--- src/components/CustomerCenter/FAQ/Faq.tsx
+++ src/components/CustomerCenter/FAQ/Faq.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import data from '../../../db.json';
 
 const Faq: any = () => {
-  return <div>Faq</div>;
+  const faq = data.FAQ;
+
+  return (
+    <>
+      {faq.map((qa) => {
+        return (
+          <div>
+            <h4>{qa.Q}</h4>
+            <p>{qa.A}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Faq;
