6e3427639d1dad00026cec6f746929c59aa934f1
Jieun Han <yjyyls66@naver.com>
2023-02-12 22:39:51 +0900

    fix:신규스토어제보, 정보오류제보 json(post)으로 코드 변경, NewStore.tsx 삭제/NewStoreReport.tsx 컴포넌트 생성

--- src/components/CustomerCenter/StoreReport/InfoError.tsx
+++ src/components/CustomerCenter/StoreReport/InfoError.tsx
@@ -2,115 +2,166 @@ import React from 'react';
 import { useState } from 'react';
 import { InfoErrorForm, ErrorImgLabel } from './style';
 import { BiImageAdd } from 'react-icons/bi';
-import { db, storage } from '../../../services/firebase';
-import { setDoc, doc } from 'firebase/firestore';
+import { storage } from '../../../services/firebase';
 import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { auth } from '../../../services/firebase';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+interface InfoErrInput {
+  title: string;
+  storeName: string;
+  infoErrContent: string;
+  infoModifiContent: string;
+}
 
 // 정보 오류/수정 제보
 const InfoError: any = () => {
-  const [errorStoreName, setErrorStoreName] = useState<string>(''); // 브랜드명
-  const [errorContent, setErrorContent] = useState<string>(''); // 오류수정건의
+  const initInfoErrModifiInput = {
+    title: '',
+    storeName: '',
+    infoErrContent: '',
+    infoModifiContent: '',
+  };
+
+  const [infoErrModifiInput, setInfoErrModifiInput] = useState<InfoErrInput>(
+    initInfoErrModifiInput,
+  );
   const [errImgFile, setErrImgFile] = useState(''); // 이미지 파일
   const [errFileName, setErrFileName] = useState(''); //이미지 파일 이름
 
-  // 브랜드명 onChange함수
-  const ErrStoreNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setErrorStoreName(event.target.value);
-  };
+  const userId = auth?.currentUser;
 
-  // 오류 수정 건의 onChange함수
-  const ErrContentHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setErrorContent(event.target.value);
+  // input onChange 함수
+  const infoErrModifiOnChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setInfoErrModifiInput({
+      ...infoErrModifiInput,
+      [event.target.name]: event.target.value,
+    });
   };
 
   // 이미지 파일 input onChange 함수
-  const errImgHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const errModifiImgOnChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     const target = event.currentTarget;
     const theFile = (target.files as FileList)[0]; // 이벤트로부터 파일을 얻어와서 첫 번째 파일만 받음
-
     setErrFileName(theFile.name);
 
     const reader = new FileReader();
     reader.readAsDataURL(theFile); // file 객체를 data url로 바꿔줌
+
     reader.onloadend = (finishedEvent: any) => {
       setErrImgFile(finishedEvent.currentTarget.result);
     };
   };
 
-  // 제보하기 버튼 클릭 시 파이어베이스에 데이터 업로드
-  const errInfoAddHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+  // // 제보하기 버튼 onSubmit 함수 (json db 추가)
+  const errModifiInfoAddHandler = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ) => {
     event.preventDefault();
 
-    const errImgRef = ref(storage, `errInfoImg/${errFileName}`);
+    // firebase storage에 이미지 업로드
+    const errInfoImgRef = ref(storage, `errInfoImg/${errFileName}`);
 
     let downloadImgUrl;
     if (errImgFile) {
-      const response = await uploadString(errImgRef, errImgFile, 'data_url');
+      const response = await uploadString(
+        errInfoImgRef,
+        errImgFile,
+        'data_url',
+      );
       downloadImgUrl = await getDownloadURL(response.ref);
     } else {
       downloadImgUrl = '';
     }
 
-    const newErrorInfo = {
-      errorStoreName,
-      errorContent,
-      errInfoImg: downloadImgUrl,
+    // 제보 날짜 받기 위해 today라는 변수 생성
+    const today = new Date();
+
+    
+    // db에 올라가는 데이터 구조
+    const newErrModifiInfo = {
+      id: uuidv4(),
+      userId,
+      title: infoErrModifiInput.title,
+      storeName: infoErrModifiInput.storeName,
+      infoErrContent: infoErrModifiInput.infoErrContent,
+      infoModifiContent: infoErrModifiInput.infoModifiContent,
+      errImg: downloadImgUrl,
+      date: today.toLocaleString(),
     };
 
+    // db에 추가
     try {
-      await setDoc(doc(db, 'newError', uuidv4()), newErrorInfo);
-      setErrorStoreName('');
-      setErrorContent('');
+      axios.post(
+        'http://localhost:3001/infoErrModifiContents',
+        newErrModifiInfo,
+      );
+      setInfoErrModifiInput(initInfoErrModifiInput);
       setErrImgFile('');
 
-      alert('제보 완료');
+      alert('제보 완료!');
     } catch (err) {
-      alert(err);
+      console.log(err);
     }
   };
 
   return (
-    <>
-      <InfoErrorForm onSubmit={errInfoAddHandler}>
-        <div>
-          <h2>정보 오류/수정 제보</h2>
-          <h4>1. 브랜드명 (스토어 이름)</h4>
-          <input
-            type="text"
-            value={errorStoreName}
-            onChange={ErrStoreNameHandler}
-          />
-        </div>
-        <div>
-          <h4>2. 오류 & 수정 건의</h4>
-          <input
-            type="text"
-            value={errorContent}
-            onChange={ErrContentHandler}
-          />
-        </div>
-        <div style={{ marginTop: 20 }}>
-          <ErrorImgLabel htmlFor="errorImg">
-            <BiImageAdd />
-            {errImgFile && (
+    <InfoErrorForm onSubmit={errModifiInfoAddHandler}>
+      <div>
+        <h2>제목</h2>
+        <input
+          type="text"
+          name="title"
+          onChange={infoErrModifiOnChangeHandler}
+          value={infoErrModifiInput.title}
+        />
+        <h2>브랜드명 (스토어 이름)</h2>
+        <input
+          type="text"
+          name="storeName"
+          onChange={infoErrModifiOnChangeHandler}
+          value={infoErrModifiInput.storeName}
+        />
+        <h2>정보 오류 내용</h2>
+        <input
+          type="text"
+          name="infoErrContent"
+          onChange={infoErrModifiOnChangeHandler}
+          value={infoErrModifiInput.infoErrContent}
+        />
+        <h2>정보 수정 내용</h2>
+        <input
+          type="text"
+          name="infoModifiContent"
+          onChange={infoErrModifiOnChangeHandler}
+          value={infoErrModifiInput.infoModifiContent}
+        />
+      </div>
+      <div style={{ marginTop: 20 }}>
+        <ErrorImgLabel htmlFor="storeInfoImg">
+          <BiImageAdd />
+          {errImgFile && (
               <img src={errImgFile} style={{ width: 150, height: 150 }} />
             )}
-          </ErrorImgLabel>
-          <input
-            type="file"
-            accept="image/*"
-            id="errorImg"
-            onChange={errImgHandler}
-            style={{ display: 'none' }}
-          />
-        </div>
-        <div>
-          <button>취소</button>
-          <button type="submit">제보하기</button>
-        </div>
-      </InfoErrorForm>
-    </>
+        </ErrorImgLabel>
+        <input
+          type="file"
+          accept="image/*"
+          id="storeInfoImg"
+          onChange={errModifiImgOnChangeHandler}
+          style={{ display: 'none' }}
+        />
+      </div>
+      <div>
+        <button>취소</button>
+        <button type="submit">제보하기</button>
+      </div>
+    </InfoErrorForm>
   );
 };
 
--- src/pages/CustomerCenterPage/CustomerCenterPage.tsx
+++ src/pages/CustomerCenterPage/CustomerCenterPage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Faq from '../../components/CustomerCenter/FAQ/Faq';
 import InfoError from '../../components/CustomerCenter/StoreReport/InfoError';
-import NewStore from '../../components/CustomerCenter/StoreReport/NewStore';
+import NewStoreReport from '../../components/CustomerCenter/StoreReport/NewStoreReport';
 
 const CustomerCenterPage: any = () => {
   return (
     <div>
-      CustomerCenterPage
-      {/* <NewStore /> */}
-      {/* <InfoError /> */}
-      <Faq />
+      <InfoError />
+      {/* <Faq /> */}
+      {/* <NewStoreReport /> */}
     </div>
   );
 };
