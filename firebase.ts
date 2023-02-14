a701591368dde09ddb6e654425adcc0c72bc3cbd
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-02-14 10:24:34 +0900

    Revert "Feat:my page profile upload"

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
@@ -15,7 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// auth 이미했음
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+// export const provider = new GoogleAuthProvider();
+// export const providerGithub = new GithubAuthProvider();
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import './App.css';
 import Router from './shared/Router';
 
 function App() {
