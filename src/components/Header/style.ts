7954d0b1145d10695b183ff82b6f3cd634b1d89a
heerachoi <heera7722@gmail.com>
2023-02-15 00:53:51 +0900

    Style: navbar alarm message

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -19,7 +19,8 @@ export const Title = styled.button`
   background-color: rgba(255, 255, 255, 0);
 `;
 
-export const DummyBox = styled.div``;
+export const DummyBox = styled.div`
+background-color: aliceblue`;
 
 export const BtnWrap = styled.div`
   width: 400px;
@@ -35,3 +36,78 @@ export const CategoryBtn = styled.button`
 `;
 
 export const MapBtn = styled(CategoryBtn)``;
+
+
+// 알리 모달
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  width: 100vw;
+  height: 100vh;
+  `;
+
+export const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1050;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+`
+
+export const ModalHolder = styled.div`
+  z-index: 999;
+  background-color: #fff;
+  position: relative;
+  margin: 1.75rem auto;
+  border-radius: 3px;
+  max-width: 500px;
+  padding: 2rem;
+`
+
+export const ModalHeader = styled.div`
+ display: flex;
+  justify-content: flex-end;
+`
+
+export const LocationModalContent = styled.div`
+`
+
+export const CloseModal = styled.div`
+  cursor: pointer;
+  background-color: aliceblue;
+`
+
+
+// 위로 안쓸듯? 
+
+export const FilterTitle = styled.h2`
+`
+
+export const FilterContainer = styled.div`
+  border: 9px solid aliceblue;
+  padding: 20px;
+`
+
+export const CategoryItemTitle = styled.h3`
+`
+
+export const CategoryItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap:wrap;
+  justify-content: space-between;
+`
+
+export const CategoryItem = styled.div`
+  /* width: 100%; */
+  padding: 10px 20px;
+  border: 9px solid aliceblue;
+  margin-bottom: 10px;
+  
+`
