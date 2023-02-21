6a572ffb250f53661c1783b6befe261fef315b70
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-02-21 21:58:08 +0900

    회원가입 홈페이지 UI 완성

--- src/components/Authentication/SignUp/style.ts
+++ src/components/Authentication/SignUp/style.ts
@@ -1,5 +1,9 @@
+import Select from 'react-select';
 import styled from 'styled-components';
+import arrow from '../../../assets/Logo/🦆 icon _circle chevron down_.png';
+import calendar from '../../../assets/Logo/🦆 icon _calendar_.png';
 
+// 컴포넌트 전체 박스
 export const Wrap = styled.div`
   box-sizing: border-box;
   display: flex;
@@ -7,18 +11,23 @@ export const Wrap = styled.div`
   align-items: center;
   flex-direction: column;
 `;
+
+// 타이틀
 export const TitleWrap = styled.div`
   margin-top: 50px;
   margin-bottom: 50px;
 `;
 export const Title = styled.span`
+  position: relative;
+  top: -8px;
   font-family: 'Apple SD Gothic Neo';
   font-style: normal;
   font-weight: 700;
-  font-size: 36px;
+  font-size: 32px;
   color: #323232;
 `;
 
+// Form 박스, Form 안의 아이템, Form에서 사용하는 버튼, Input, 텍스트 등
 export const FormWrap = styled.form``;
 export const FormItemWrap = styled.div`
   display: flex;
@@ -34,21 +43,49 @@ export const FormText = styled(Title)`
   font-weight: 600;
   font-size: 16px;
   margin-bottom: 10px;
+  top: 0;
 `;
 
 export const FormInput = styled.input`
-  border: 1px solid #a6a6a6;
+  border: 1px solid #bdbdbd;
   border-radius: 8px;
-  width: 400px;
-  height: 50px;
+  width: 367px;
+  height: 60px;
   padding: 0 20px 0 20px;
+  color: #323232;
 
+  &::placeholder {
+    color: #9b9b9b;
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid #676767;
+  }
+`;
+
+export const FormDate = styled(FormInput)`
+  position: relative;
+  cursor: pointer;
+  // 이미지 오른쪽에 두기, 전체 인풋 클릭시 date 나오게 하기
+  background: url(${calendar}) no-repeat 95% 50%/15px auto;
+  &::-webkit-calendar-picker-indicator {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    color: transparent;
+    cursor: pointer;
+    z-index: 1;
+  }
+  // placeholder 설정하려면 필요한 css
   &::before {
     content: attr(data-placeholder);
     width: 100%;
     font-family: 'Apple SD Gothic Neo';
     font-style: normal;
-    color: gray;
+    color: #9b9b9b;
   }
   &:valid::before {
     display: none;
@@ -56,40 +93,79 @@ export const FormInput = styled.input`
 `;
 
 export const FormSelect = styled(FormInput)`
-  width: 442px;
+  width: 409px;
+  color: #9b9b9b;
+  // 버튼 없애고 새로운 이미지 첨부
+  appearance: none;
+  background: url(${arrow}) no-repeat 95% 50%/15px auto;
+  // value='', disabled 인 거 display 없애기
+  option[value=''][disabled] {
+    display: none;
+  }
+  ///////// 값이 안먹힘 //////////
+  option {
+    background: lightcoral;
+    color: #fff;
+    padding: 3px 0;
+    font-size: 16px;
+    width: 200px;
+    height: 300px;
+  }
 `;
 
 export const PhoneInput = styled(FormInput)`
-  width: 250px;
+  width: 230px;
 `;
 
 export const PhoneBtn = styled.button`
-  background-color: #a6a6a6;
   border-radius: 8px;
-  border: none;
-  color: white;
   width: 130px;
-  height: 50px;
+  height: 60px;
   font-family: 'Apple SD Gothic Neo';
   font-style: normal;
   font-weight: 500;
   font-size: 14px;
   cursor: pointer;
+  color: #9b9b9b;
+  background-color: #f5f5f5;
+  border: 1px solid #d9d9d9;
+  &:hover:enabled {
+    background-color: #d9d9d9;
+    border: 1px solid #bdbdbd;
+  }
 `;
 
 export const CancleBtn = styled(PhoneBtn)`
-  width: 210px;
-  color: #323232;
-  background-color: white;
-  border: 1px solid #323232;
+  width: 200px;
+  color: #9b9b9b;
+  background-color: #f5f5f5;
+  border: 1px solid #d9d9d9;
+  &:hover:enabled {
+    background-color: #d9d9d9;
+    border: 1px solid #bdbdbd;
+  }
 `;
 
 export const SignUpBtn = styled(CancleBtn)`
-  color: white;
-  background-color: #323232;
+  color: #ffffff;
+  background-color: #bdbdbd;
+  border: 1px solid #9b9b9b;
+  &:hover:enabled {
+    background-color: #676767;
+    border: 1px solid #323232;
+  }
 `;
 
 export const HelperText = styled.span`
   color: red;
   margin-top: 5px;
+`;
+
+export const TextBackground = styled.div`
+  text-align: center;
+  width: 140px;
+  height: 30px;
+  background-color: #ffeb62;
+  box-sizing: border-box;
+  border-radius: 20px;
 `;
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,7 +3,7 @@ import * as S from './style';
 import { auth } from '../../../services/firebase';
 import Logout from '../../Authentication/Logout/Logout';
 import { useRecoilValue } from 'recoil';
-import { userInfo } from '../../../atoms';
+import { globalBtn, userInfo } from '../../../atoms';
 import { BsBellFill } from 'react-icons/bs';
 import useModal from '../../../hooks/useModal';
 import Modal from '../../SearchPage/SearchModal/SearchModal';
@@ -13,15 +13,17 @@ import styled from 'styled-components';
 const Header = () => {
   const navigate = useNavigate();
   const user = useRecoilValue(userInfo);
+  const globalButton = useRecoilValue(globalBtn);
 
   console.log(auth?.currentUser?.email); //header에서만 null이 뜬다. 헤더가 먼저 렌더링 되서 console에 null이 떳다가 렌더링이 다되면 null이 안뜸.
   // 해결 : Router에서 auth.onAuthStateChanged 메서드를 사용해서 파이어베이스에서 DB정보를 참조해서 변경 사항 가져옴
 
-  const notification = () => {
-    console.log('alert');
-  };
-
   const { isShowing, toggle } = useModal();
+
+  const GlobalBtnChangehandler = (page: string) => {
+    if (globalButton) return alert('멈춰!');
+    navigate(page);
+  };
 
   return (
     <>
@@ -31,13 +33,13 @@ const Header = () => {
             className="title"
             src={require('../../../assets/Logo/popcorn_logo.png')}
             alt="타이틀"
-            onClick={() => navigate('/')}
+            onClick={() => GlobalBtnChangehandler('/')}
           />
           <TitleImg
             className="TitleImg"
             src={require('../../../assets/Logo/State=Hovered.png')}
             alt="타이틀"
-            onClick={() => navigate('/')}
+            onClick={() => GlobalBtnChangehandler('/')}
           />
         </HoverBox>
         <S.BtnWrap>
@@ -52,13 +54,13 @@ const Header = () => {
             </MenuImageBackground>
           )}
           <TextBackground>
-            <S.CategoryBtn onClick={() => navigate('/report')}>
+            <S.CategoryBtn onClick={() => GlobalBtnChangehandler('/report')}>
               팝업스토어 제보
             </S.CategoryBtn>
           </TextBackground>
           {!user.isLogin && (
             <TextBackground style={{ width: 110 }}>
-              <S.CategoryBtn onClick={() => navigate('/signup')}>
+              <S.CategoryBtn onClick={() => GlobalBtnChangehandler('/signup')}>
                 회원가입
               </S.CategoryBtn>
             </TextBackground>
@@ -67,7 +69,7 @@ const Header = () => {
             <Logout />
           ) : (
             <TextBackground style={{ width: 100 }}>
-              <S.CategoryBtn onClick={() => navigate('/login')}>
+              <S.CategoryBtn onClick={() => GlobalBtnChangehandler('/login')}>
                 로그인
               </S.CategoryBtn>
             </TextBackground>
@@ -77,8 +79,8 @@ const Header = () => {
               <S.CategoryBtn
                 onClick={() => {
                   user.userInfomation.email === 'master@gmail.com'
-                    ? navigate('/master')
-                    : navigate('/my');
+                    ? GlobalBtnChangehandler('/master')
+                    : GlobalBtnChangehandler('/my');
                 }}
               >
                 마이페이지
@@ -89,7 +91,7 @@ const Header = () => {
             <MenuImage
               src={require('../../../assets/Logo/Spot.png')}
               alt="타이틀"
-              onClick={() => navigate('/map')}
+              onClick={() => GlobalBtnChangehandler('/map')}
             />
           </MenuImageBackground>
         </S.BtnWrap>
