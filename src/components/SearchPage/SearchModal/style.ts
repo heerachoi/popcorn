e7c818e29d1017c698ab5b49b85036abac5f57ab
heerachoi <heera7722@gmail.com>
2023-02-21 09:37:31 +0900

    style: reset css

--- src/components/SearchPage/SearchModal/style.ts
+++ src/components/SearchPage/SearchModal/style.ts
@@ -10,7 +10,7 @@ export const ModalContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #000;
-  opacity: .5;
+  opacity: .8;
 `
 
 export const ModalWrapper = styled.div`
@@ -29,15 +29,16 @@ export const ModalHolder = styled.div`
   z-index: 100;
   background: white;
   position: relative;
-  margin: 1.75rem auto;
+  margin: .5rem auto;
   border-radius: 3px;
   max-width: 500px;
   padding: 2rem;
+  background-color: aliceblue;
 `
 
 // Modal 
 export const ModalHeader = styled.div`
- display: flex;
+  display: flex;
   justify-content: flex-end;
 `
 
@@ -46,18 +47,17 @@ export const LocationModalContent = styled.div`
 
 export const CloseModal = styled.div`
   cursor: pointer;
-  background-color: aliceblue;
+  
 `
-
 export const FilterTitle = styled.h2`
-  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  margin-top: -0.8rem;  
 `
 
 export const FilterContainer = styled.div`
-  border: 2px solid #9AF8FF;
   padding: 20px 30px 30px;
   box-sizing: border-box;
-
 `
 
 export const CategoryItemTitle = styled.h3`
@@ -72,12 +72,15 @@ export const CategoryItemContainer = styled.div`
 `
 
 export const CategoryButton = styled.div<CategoryItemProps>`
-  /* width: 100%; */
-  padding: 10px 20px;
-  border: 1px solid #9AF8FF;
-  border-radius: 10px;
-  margin-bottom: 10px;
-  background-color: ${props => props.active ? '#E4FDFF':'tranparent'  };
+  display: flex;
+  align-items: center;
+  padding: 4px 16px 3px;
+  gap: 8px;
+  font-weight: 700;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 20px;
+  background-color: ${props => props.active ? '#FFB321':'#676767'  };
   cursor : pointer ;
 `
 
@@ -85,7 +88,11 @@ export const CloseButton = styled.button`
   border: none;
   font-size: 30px;
   background-color: transparent;
+  color: #9B9B9B;
   cursor : pointer ;
+  &:hover {
+    color: #000;
+  }
 `
 
 export const ButtonContainer = styled.div`
@@ -111,4 +118,28 @@ export const CancelButton = styled.button`
 `
 
 export const SubmitButton = styled(CancelButton)`
+  box-sizing: border-box;
+
+/* Auto layout */
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 8px;
+gap: 8px;
+
+position: absolute;
+width: 160px;
+height: 40px;
+left: calc(50% - 160px/2 + 88px);
+bottom: 48px;
+
+/* Grayscale/Gray4 */
+
+background: #BDBDBD;
+/* Grayscale/Gray3 */
+
+border: 1px solid #9B9B9B;
+border-radius: 8px;
 `
