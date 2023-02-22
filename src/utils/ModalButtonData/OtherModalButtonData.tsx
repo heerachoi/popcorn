6a2a98505905c55aca4379d1b02b5de117390560
heerachoi <heera7722@gmail.com>
2023-02-22 16:29:10 +0900

    fix: data women men -> 여성, 남성으로 변경

--- src/utils/ModalButtonData/OtherModalButtonData.tsx
+++ src/utils/ModalButtonData/OtherModalButtonData.tsx
@@ -8,8 +8,8 @@ export const OtherModalButtonData = atom({
     { id: 3, label: '20', active: false },
     { id: 4, label: '30', active: false },
     { id: 5, label: '40+', active: false },
-    { id: 6, label: 'women', active: false },
-    { id: 7, label: 'men', active: false },
+    { id: 6, label: '여성', active: false },
+    { id: 7, label: '남성', active: false },
     { id: 8, label: '연령모름', active: false },
     { id: 9, label: '성별모름', active: false },
   ]
