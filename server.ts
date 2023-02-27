555a5c7aa6356c0b1c2110e28f75db75f383be27
heerachoi <heera7722@gmail.com>
2023-02-27 20:32:43 +0900

    feat: removed server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,26 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// import express from 'express';
+// import path from 'path';
+// const app = express();
+// const path = require('path');
 
-server.use(middlewares);
-server.use(router);
 
-server.listen(port,()=>{
-  console.log('서버가 열렸습니다.')
-});
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port,()=>{
+//   console.log('서버가 열렸습니다.')
+// });
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('frontend/build'))
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     })
+// }
