e880c46aaa91202f8213b9a0dfdc9b660f180c61
heerachoi <heera7722@gmail.com>
2023-02-27 14:39:38 +0900

    refactor: data import with useQuery

--- src/components/HomePage/HomePageList/HomePageList.tsx
+++ src/components/HomePage/HomePageList/HomePageList.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import data from '../../../data/popupStore.json';
+import { getPopupData } from '../../../services/api';
+import { Store } from '../../../types/data/storeInterface';
 import {
   StyleListWrap,
   RecentList,
@@ -11,6 +13,7 @@ import {
 
 const HomePageList: any = () => {
   const navigate = useNavigate();
+  const { data } = useQuery('popup', getPopupData);
 
   // yyyymmdd 포맷으로 오늘 날짜를 받아오는 함수
   const currentDate = new Date();
@@ -18,7 +21,7 @@ const HomePageList: any = () => {
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const day = String(currentDate.getDate()).padStart(2, '0');
   const today = parseInt(year + month + day);
-  const popupList = data.Store.filter((list) => {
+  const popupList = data.Store.filter((list:Store) => {
     return (
       parseInt(list.open.split('.').join('')) >= today - 5 &&
       today >= parseInt(list.open.split('.').join(''))
@@ -27,7 +30,7 @@ const HomePageList: any = () => {
   return (
     <>
       <RecentList>최근 오픈했어요</RecentList>
-      {popupList.map((popup) => {
+      {popupList.map((popup:Store) => {
         return (
           <>
             <StyleListWrap
