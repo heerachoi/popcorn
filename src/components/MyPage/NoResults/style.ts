05d00f81c421a76b967d0379598100e6a211f45a
Jieun Han <yjyyls66@naver.com>
2023-02-28 15:00:03 +0900

    feat:나의 쓴 제보 결과 없음 컴포넌트 생성, 이미지 추가 및 코드 수정

---
+++ src/components/MyPage/NoResults/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ResultWarp = styled.div``;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 341px;
+`;
+
+export const ResultText = styled.p`
+  font-family: 'Apple SD Gothic Neo';
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: center;
+`;
+
+export const ResultImg = styled.img`
+
+`
