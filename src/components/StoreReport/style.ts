02a1c9985be4e851877f625c56609a3876449139
innasz <mini5167@naver.com>
2023-03-02 21:15:40 +0900

    feat:kakaologin ing & daum modal 수정

--- src/components/StoreReport/style.ts
+++ src/components/StoreReport/style.ts
@@ -1,4 +1,4 @@
-// import DaumPostcode from 'react-daum-postcode';
+import DaumPostcode from 'react-daum-postcode';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styled from 'styled-components';
@@ -230,6 +230,4 @@ export const ErrorImgLabel = styled.label`
 
 export const PostModal = styled.div``;
 
-// export const DaumPostcodeModal = styled(DaumPostcode)`
-
-// `;
+export const DaumPostcodeModal = styled(DaumPostcode)``;
