ceb513b116289b18f2c75adf521bcce9cbff78d0
Jieun Han <yjyyls66@naver.com>
2023-03-03 10:13:48 +0900

    fix: 무한스크롤 구현 중단

--- src/components/MyPage/BookMarkList/BookMarkList.tsx
+++ src/components/MyPage/BookMarkList/BookMarkList.tsx
@@ -1,61 +1,17 @@
 import { useQuery } from 'react-query';
 import * as S from './style';
-import { getBookMark, JSON_API } from '../../../services/api';
+import { getBookMark } from '../../../services/api';
 import { useRecoilValue } from 'recoil';
 import { userInfo } from '../../../atoms';
 import BookmarkNoResult from '../NoResults/BookmarkNoResult';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import axios from 'axios';
-import { useInView } from 'react-intersection-observer';
 
 const BookMarkList = () => {
-  const observerRef = useRef<HTMLDivElement>(null);
-  const [page, setPage] = useState<number>(1);
-  const [item, setItem] = useState<any>([]);
-  const [loading, setLoaging] = useState(false);
   const user = useRecoilValue(userInfo);
   const userInfos = user.userInfomation;
 
-  const { data, isLoading } = useQuery(
-    ['BookMarkList', page],
-    () => getBookMark(page),
-    {
-      keepPreviousData: true, // 이전 데이터 유지
-    },
-  );
+  const { data, isLoading } = useQuery('BookMarkList', getBookMark);
 
-  const loadMoreHandler = async () => {
-    try {
-      setItem([...item, ...data]);
-      setPage((prev) => prev + 1);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    console.log('렌더링 됨');
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.7,
-    };
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          loadMoreHandler();
-        }
-      });
-    }, options);        
-    if (observerRef.current) observer.observe(observerRef.current);
-    return () => observer.disconnect();
-  }, [loadMoreHandler]);
-
-  useEffect(() => {
-    setLoaging(isLoading);
-  }, [isLoading]);
-
-  if (isLoading && page === 1) {
+  if (isLoading) {
     console.log('로딩중');
     return <p>Loading...</p>;
   }
@@ -89,7 +45,6 @@ const BookMarkList = () => {
               </S.BookMarkCard>
             );
           })}
-          <div ref={observerRef}>여기!!</div>
         </S.BookMarkContainer>
       )}
     </>
