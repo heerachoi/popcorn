c96a72ca8877bb349e183b52b2f3d1b5d22df5a7
innasz <mini5167@naver.com>
2023-03-07 23:37:42 +0900

    fix:kakao redirect uri 수정

--- src/components/Authentication/Login/KakaoLogin.tsx
+++ src/components/Authentication/Login/KakaoLogin.tsx
@@ -24,7 +24,7 @@ interface UserInfo {
 const KakaoLogin = () => {
   const location = useLocation(); // useLocation hook 사용
   const REACT_APP_REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  const REACT_APP_REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+  const REACT_APP_REDIRECT_URI = 'https://popcorn-hazel.vercel.app/login';
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REACT_APP_REST_API_KEY}&redirect_uri=${REACT_APP_REDIRECT_URI}&response_type=code`; // 인가코드 요청 URL
   const REACT_APP_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET; // 카카오 디벨로퍼스에서 발급받은 client secret 키
   //주소창에 파라미터code를 가져온다 split 메서드를 활용한다
